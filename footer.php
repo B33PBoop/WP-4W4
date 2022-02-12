1cd6fca7560b14e52fcb69a46d066fc3b0f1af5d
Jean-Romain <jromainroy@gmail.com>
2022-02-11 22:17:16 -0500

    Finition des styles du header, début du footer

--- footer.php
+++ footer.php
@@ -1,8 +1,19 @@
 <footer class="site__footer">
-    <h2 class="footer__titre">Le beau footer</h2>
-    <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
-    <h3 class="footer__author">Thème par Florence Lemieux Cayer, Adaptation par Jean-Romain Roy</h3>
-    <?php wp_nav_menu(array("menu" =>"simple", "container" => "nav", "container_class" =>"footer")) ?>
+    <div class="footer__info">
+        <p>3800, Rue Sherbrooke Est \n Montréal</p>
+        <p>(Québec) H1X 2A2</p>
+        <p>514-254-7131</p>
+        <p>Un site web créé par des étudiants du programme TIM</p>
+        <p>Thème par Florence Lemieux Cayer, Adaptation par Jean-Romain Roy</p>
+    </div>
+
+    <div class="footer__liens">
+        <?php wp_nav_menu(array("menu" =>"simple", "container" => "nav", "container_class" =>"footer")) ?>
+    </div>
+
+    <div class="footer__lorem">à
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet egestas tellus, ut porttitor diam placerat non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla pellentesque arcu a nisi euismod, at placerat nulla vestibulum. Curabitur pulvinar convallis nisl ac iaculis. Vestibulum lacinia aliquet dui sed posuere. Curabitur placerat turpis ac augue tempor tincidunt a id sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+    </div>
 </footer>
 </body>
 
