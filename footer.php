e32010d0fa95819969573bb4ca44445714dd9e94
Jean-Romain <jromainroy@gmail.com>
2022-02-11 22:36:33 -0500

    Finition du footer/styles, passage au retouches

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer class="site__footer">
+
     <div class="footer__info">
-        <p>3800, Rue Sherbrooke Est \n Montréal</p>
+        <p>3800, Rue Sherbrooke Est Montréal</p>
         <p>(Québec) H1X 2A2</p>
         <p>514-254-7131</p>
         <p>Un site web créé par des étudiants du programme TIM</p>
@@ -11,7 +12,7 @@
         <?php wp_nav_menu(array("menu" =>"simple", "container" => "nav", "container_class" =>"footer")) ?>
     </div>
 
-    <div class="footer__lorem">à
+    <div class="footer__lorem">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet egestas tellus, ut porttitor diam placerat non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla pellentesque arcu a nisi euismod, at placerat nulla vestibulum. Curabitur pulvinar convallis nisl ac iaculis. Vestibulum lacinia aliquet dui sed posuere. Curabitur placerat turpis ac augue tempor tincidunt a id sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
     </div>
 </footer>
