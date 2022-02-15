f9eb5c19601522bb39120d62b38c540d76b26c27
Jean-Romain Roy <e1959009@cmaisonneuve.qc.ca>
2022-02-15 10:24:13 -0500

    Premier commit, début d'une nouvelle branche(?)

---
+++ README.md
@@ -0,0 +1,11 @@
+# Exercice #1  - 4W4 - Interfaces et développement web
+### Auteur - *Jean-Romain Roy*
+### Date de remise - 11/2/2022
+
+```
+Cet exercice nous à permi d'introduire la structure d'un thème WordPress. 
+Voici la structure utilisée:
+    - Pour le style css nous avons utilisés Sass
+    - Les fonctions du thème se trouvent dans functions.php
+    - Le modèle principal est Index.php
+```
---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer class="site__footer">
+
+    <div class="footer__info">
+        <p>3800, Rue Sherbrooke Est Montréal</p>
+        <p>(Québec) H1X 2A2</p>
+        <p>514-254-7131</p>
+        <p>Un site web créé par des étudiants du programme TIM</p>
+        <p>Thème par Florence Lemieux Cayer, Adaptation par Jean-Romain Roy</p>
+    </div>
+
+    <div class="footer__liens">
+        <?php wp_nav_menu(array("menu" =>"simple", "container" => "nav", "container_class" =>"footer")) ?>
+    </div>
+
+    <div class="footer__lorem">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus aliquet egestas tellus, ut porttitor diam placerat non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla pellentesque arcu a nisi euismod, at placerat nulla vestibulum. Curabitur pulvinar convallis nisl ac iaculis. Vestibulum lacinia aliquet dui sed posuere. Curabitur placerat turpis ac augue tempor tincidunt a id sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+    </div>
+</footer>
+</body>
+
+</html>
