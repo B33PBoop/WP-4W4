a07fca932ca170829235d40379279047837f3ca2
Jean-Romain Roy <e1959009@cmaisonneuve.qc.ca>
2022-02-22 09:34:58 -0500

    Début de la nouvelle branche

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lab 5  - 4W4 - Interfaces et développement web
+### Auteur - *Jean-Romain Roy*
+### Date de remise - 18/2/2022
+
---
+++ page.php
@@ -0,0 +1,14 @@
+<?php get_header() ?>
+<main class="principal">
+    <section class="site__main">
+    <h1>Page.php</h1>
+        <section class="formation">
+            <h2 class="formation__titre">Page Générale</h2>
+                <?php if (have_posts()): the_post();?>
+                    <?php the_title(); ?>
+                    <?php the_content(); ?>
+                <?php endif ?>
+        </section>
+    </section>
+</main>
+<?php get_footer() ?>
