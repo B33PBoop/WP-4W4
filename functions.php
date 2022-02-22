50690877159b28b80f92cd4c6100b7cf7d6ddf0b
Jean-Romain Roy <e1959009@cmaisonneuve.qc.ca>
2022-02-22 11:36:27 -0500

    ajout de thumbnails / template single-post

--- functions.php
+++ functions.php
@@ -23,5 +23,8 @@ function cidw_4w4_filtre_choix_menu($obj_menu){
     }
     return $obj_menu;
 }
-add_filter("wp_nav_menu_objects", "cidw_4w4_filtre_choix_menu")
+add_filter("wp_nav_menu_objects", "cidw_4w4_filtre_choix_menu");
+
+/*Ajout de supports*/
+add_theme_support( 'post-thumbnails' );
 ?>
