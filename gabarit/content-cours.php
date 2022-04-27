eb9f64a1063d5ed1daad7c8222dc1b9e6b25f876
Jean-Romain <jromainroy@gmail.com>
2022-04-26 20:06:04 -0400

    implémentation custom-field pour le département

--- gabarit/content-cours.php
+++ gabarit/content-cours.php
@@ -5,8 +5,8 @@
                         $titre = get_the_title();
                         $titreFiltreCours = substr($titre, 7, -6);
                         // $nbHeures = substr($titre, -6);
-                        $nbHeures = get_field('nombre_dheure');
-                        $sigleCours = substr($titre, 0, 7);
+                        $nbHeures = get_field("nombre_dheures");
+                        $sigleCours = substr($titre, 4, 3);
                         $departement = get_field("departement");
                         //^^ voir vidéos pour trouver ou placer cette variable^^
                         // $descCours = get_the_excerpt();
@@ -23,7 +23,7 @@
                                 <?= $titreFiltreCours; ?>
                             </a>
                         </h3>
-                        <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
+                        <div class="cours__nbre-heure"><?= $nbHeures; ?>h</div>
                         <p class="cours__sigle"><?= $sigleCours; ?> </p>
                         <p class="cours__desc"> <?= wp_trim_words($descCours, 20, '<button class="cours__desc__suite"> la suite </button>');?></p>
                         <p class="cours__dep"><?= $departement ?></p>
