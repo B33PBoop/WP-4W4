0a21ced5ea1535d0a5029c8716016001531b0eb9
Jean-Romain <jromainroy@gmail.com>
2022-04-29 15:06:28 -0400

    update checkbox pour utiliser des clip-paths

--- header.php
+++ header.php
@@ -26,8 +26,10 @@
 </header>
 <section class="site__barre">
     <input type="checkbox" id="chk-burger">
-    <label for="chk-burger" id="burger">
-        <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+    <label for="chk-burger" id="burger" class="burger">
+        <div class="burger__ligne"></div>
+        <div class="burger__ligne"></div>
+        <div class="burger__ligne"></div>
      </label>
     <?php 
     $icone= '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" color="#ffffff"><path fill-rule="evenodd" d="M8.72 18.78a.75.75 0 001.06 0l6.25-6.25a.75.75 0 000-1.06L9.78 5.22a.75.75 0 00-1.06 1.06L14.44 12l-5.72 5.72a.75.75 0 000 1.06z"></path></svg>';
