990a899412e848e213e2a42a8ddbf11ba6a32ee1
Jean-Romain <jromainroy@gmail.com>
2022-05-19 13:27:22 -0400

    update de la mise en page du template "atelier"

--- template-atelier.php
+++ template-atelier.php
@@ -2,19 +2,14 @@
 <?php get_header() ?> 
 <main class="site__main">
     <?php if (have_posts()): the_post();?>
-            <?php the_title(); ?>
-            <?php 
-                $image = get_field('image');
-                if( !empty( $image ) ): ?>
-                <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-            <?php endif; ?>
+            <h2>Atelier : <?php the_title(); ?></h2>
 
-            <h3>Description de l'atelier</h3>
-            <p> <?php the_field('animateur')?></p>
-            <p> <?php the_field('local')?></p>
+            <h5>Description de l'atelier</h5>
+            <p> L'animateur de l'atelier est <?php the_field('animateur')?></p>
+            <p> L'atelier se déroulera au local <?php the_field('local')?></p>
             <p><?php the_field('description'); ?></p>
 
-            <h3>Horaires et dates de l'atelier</h3>
+            <h5>Horaires et dates de l'atelier</h5>
             <p>Durée de chacune des scéances est de <?php the_field('duree')?> heures</p>
             <p>Date de début: <?php the_field('date_de_debut'); ?></p>
             <p>Date de fin: <?php the_field('date_de_fin'); ?></p>
